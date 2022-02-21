c54c886a6d5c30fa557050ed40db79ae2df79a76
Joe Cox <joecox@live.co.uk>
2022-02-21 05:56:33 +0000

    update styles to match spec a bit more

--- components/country-preview.js
+++ components/country-preview.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function CountryPreview({ country }) {
   return (
-    <article className="mb-4 w-full p-2 sm:w-1/2 md:w-1/3 xl:mb-12 xl:w-1/4">
+    <article className="mb-4 w-full p-0 sm:w-1/2 sm:p-4 md:w-1/3 lg:p-8 xl:mb-12 xl:w-1/4">
       <div className="h-full overflow-hidden rounded-md border border-lm-light-grey bg-white shadow-lg dark:border-dm-dark-blue dark:bg-dm-blue">
         <Link href={`/${country.cca3}`}>
           <a>
@@ -21,12 +21,20 @@ export default function CountryPreview({ country }) {
           </a>
         </Link>
         <div className="bg-white px-5 py-6 text-dm-dark-blue dark:bg-dm-blue dark:text-lm-light-grey">
-          <h2 className="text-lg font-extrabold ">
+          <h2 className="mb-4 text-lg font-extrabold ">
             {<Link href={`/${country.cca3}`}>{country.name.common}</Link>}
           </h2>
           <p>
             <b>Population: </b>
-            {country.population}
+            {country.population.toLocaleString()}
+          </p>
+          <p>
+            <b>Region: </b>
+            {country.region}
+          </p>
+          <p>
+            <b>Capital: </b>
+            {country.capital}
           </p>
         </div>
       </div>
